25ffb89813b399d614c27b81db57218f006c1a18
Vamsi Dharmavarapu <vamsi.akirah@gmail.com>
2017-12-19 07:45:18 +0530

    debug

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.use(function(req, res, next){
 	  	host     : process.env.DB_HOST,
 	  	user     : process.env.DB_USER,
         password : process.env.DB_PASSWORD,
-  		database : process.env.DB_NAME
+  		database : process.env.DB_NAME,
+        port     : 3306
 	});
 	connection.connect();
 	next();
