333eb2a7695598ac8bcca25fcf815d8b2b7b7ba0
Vamsi Dharmavarapu <vamsi.akirah@gmail.com>
2017-12-21 19:47:55 +0530

    delete before update

--- routes/users.js
+++ routes/users.js
@@ -43,26 +43,36 @@ router.post('/', function(req, res, next) {
 	var employeeName = req.body.employeeName;
 	var companyID = req.body.companyID;
 
-	var promises = vehicles.map(vehicle => {
-        var query = "INSERT INTO user_Profile (userID, employeeName, companyID, vehicleType, vehicleNumber, RFID_Number) values ( " +
-           userID + ", \'" + employeeName + "\', \'" + companyID + "\', \'" + vehicle.vehicleType + "\', \'" + vehicle.vehicleNumber + "\', \'" + vehicle.RFID_Number +  "\')";
-        console.log('POST user query ::: ', query);
-        return new Promise(function(resolve, reject) {
-            connection.query(query, function (error, result, fields) {
-                if(error){
-                    reject({"status": 500, "error": error, "response": null});
-                    //If there is error, we send the error in the error section with 500 status
-                } else {
-                	resolve(result);
-                    //If there is no error, all is good and response is 200OK.
-                }
-            });
-        });
+	var deleteQuery = "DELETE FROM user_profile where userID = "+ userID ;
 
-	});
-	Promise.all(promises)
-        .then(result => res.send({"status": 200, "success": true, "user_profile": req.body}))
-        .catch(e => res.send({"status": 500, "error": e, "response": null}))
+	connection.query(deleteQuery,  (error, results, fields) => {
+        if(error){
+            res.send({"status": 500, "error": error, "response": null});
+            //If there is error, we send the error in the error section with 500 status
+        } else {
+            var promises = vehicles.map(vehicle => {
+                var query = "INSERT INTO user_Profile (userID, employeeName, companyID, vehicleType, vehicleNumber, RFID_Number) values ( " +
+                    userID + ", \'" + employeeName + "\', \'" + companyID + "\', \'" + vehicle.vehicleType + "\', \'" + vehicle.vehicleNumber + "\', \'" + vehicle.RFID_Number +  "\')";
+                console.log('POST user query ::: ', query);
+                return new Promise(function(resolve, reject) {
+                    connection.query(query, function (error, result, fields) {
+                        if(error){
+                            reject({"status": 500, "error": error, "response": null});
+                            //If there is error, we send the error in the error section with 500 status
+                        } else {
+                            resolve(result);
+                            //If there is no error, all is good and response is 200OK.
+                        }
+                    });
+                });
+
+            });
+            Promise.all(promises)
+                .then(result => res.send({"status": 200, "success": true, "user_profile": req.body}))
+                .catch(e => res.send({"status": 500, "error": e, "response": null}))
+            //If there is no error, all is good and response is 200OK.
+        }
+    });
 });
 
 module.exports = router;
